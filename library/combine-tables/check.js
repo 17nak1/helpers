0d37187eed0864bcde3c70cffb6ec4026d05f03c
Nazila <17nak1@queensu.ca>
2019-09-02 17:52:06 -0400

    library

---
+++ library/combine-tables/check.js
@@ -0,0 +1,21 @@
+fs = require('fs')
+var dataset1 = []
+var file = fs.readFileSync('./DeterministicSEIR_all.csv').toString()
+var lines = file.split('\n')
+for (let i = 1; i < lines.length; i++) {
+  dataset1.push(lines[i].split(','))
+}
+
+var dataset2 = []
+var file = fs.readFileSync('./all.csv').toString()
+var lines = file.split('\n')
+for (let i = 1; i < lines.length; i++) {
+  dataset2.push(lines[i].split(','))
+}
+for( i= 0; i < dataset1.length; i++) {
+  for (j=0; j < dataset1[0].length; j++) {
+    if (Math.abs(dataset2[i][j] - dataset1[i][j]) > 1e-9) {
+      console.log(i,j, dataset2[i][j] - dataset1[i][j])
+    }
+  }
+}
